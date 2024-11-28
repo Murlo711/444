d2ec9959e1c88ce7d95e8862a4453aa9b6fe7429
Dmitriy <radichda22@st.ithub.ru>
2024-11-28 15:40:12 +0300

    Add files via upload

---
+++ кт 4/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('Пользователь подключился');
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключился');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Сервер запущен на http://localhost:3000');
+});
+app.get('/events', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    setInterval(() => {
+        res.write(`data: Новое обновление в ${new Date()}\\n\\n`);
+    }, 5000);
+});
